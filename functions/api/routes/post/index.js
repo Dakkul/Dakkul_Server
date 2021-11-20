53ce6aa8b7ebc15c324ffa6c860f240a49d49d8b
seol <suljiwon@naver.com>
2021-11-21 06:10:10 +0900

    feat: add tagSearch router

--- functions/api/routes/post/index.js
+++ functions/api/routes/post/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.get('/list', require('./postListGET'));
 router.get('/:postId', require('./postGET'));
+router.get('/tagSearch', require('./postTagSearchGet'));
 
 module.exports = router;
