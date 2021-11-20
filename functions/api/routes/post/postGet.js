b72491dbc11c1ce1fe086c030b8a9073bfc4c0dd
seol <suljiwon@naver.com>
2021-11-21 07:55:07 +0900

    feat: add postGet tags

--- functions/api/routes/post/postGet.js
+++ functions/api/routes/post/postGet.js
@@ -1,9 +1,10 @@
+const _ = require('lodash');
 const functions = require('firebase-functions');
 const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
-const { postDB } = require('../../../db');
+const { postDB, postTagDB, tagDB } = require('../../../db');
 
 module.exports = async (req, res) => {
   const { postId } = req.params;
@@ -15,6 +16,17 @@ module.exports = async (req, res) => {
     client = await db.connect(req);
 
     const post = await postDB.getPostById(client, postId);
+    const postTags = await postTagDB.getAllPostTags(client);
+    const tags = await tagDB.getAllTags(client);
+
+    for (let i = 0; i < postTags.length; i++) {
+      postTags[i].tag = _.find(tags, (tag) => tag.id === postTags[i].tagId);
+    }
+
+    for (let i = 0; i < post.length; i++) {
+      post[i].tags = _.filter(postTags, (pt) => pt.postId === post[i].id).map((o) => o.tag);
+    }
+
     res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ONE_POST_SUCCESS, post));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
